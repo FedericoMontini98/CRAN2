62f1836bbf530fb0f317c3bf197e817835f4fdeb
ema0498 <ema.tinghi@gmail.com>
2022-02-09 19:07:34 +0100

    Update RRH.cc

--- CRAN2/src/RRH.cc
+++ CRAN2/src/RRH.cc
@@ -20,9 +20,35 @@ Define_Module(RRH);
 void RRH::initialize()
 {
     // TODO - Generated method body
+    timer_ = new cMessage("timer");
 }
 
 void RRH::handleMessage(cMessage *msg)
 {
     // TODO - Generated method body
+    if(msg->isSelfMessage()){
+        //decompression ended
+        forwardPkt(msg);
+    }
+    else{
+        if(par("compression_used")){
+            decompressPkt(msg);
+        }
+        else{
+            scheduleAt(simTime(), timer_);
+        }
+    }
+}
+
+void RRH::forwardPkt(cMessage *msg)
+{
+
+}
+
+void RRH::decompressPkt(cMessage *msg)
+{
+    PktMessage* m = check_and_cast<PktMessage*>(msg);
+    long long to_wait = 50 * par("compression_ratio");
+    simtime_t decompression_time = SimTime (to_wait, -3);
+    scheduleAt(simTime() + decompression_time,);
 }
